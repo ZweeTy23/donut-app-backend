32bad855033126cc1385e5fc8d65b90798b96db7
ZweeTy23 <josealbertodc2006@gmail.com>
2025-06-06 11:20:43 -0600

    first commit

---
+++ src/test/java/com/tecdesoftware/demo/DonutAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tecdesoftware.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonutAppBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
